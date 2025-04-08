8abb6b9dda1d9bcb2dfa1c12dfaf1a90bb33219f
Krishna <krishnagovarthini-lmnas>
2025-04-08 15:29:00 +0530

    Added placeholder animation and fixed coding standard on form API

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -69,8 +69,8 @@ export default function ContactPage() {
                                         onClick={() => fnSetContactMessage("")}
                                         className="absolute top-2 right-2 text-green-700 hover:text-green-900"
                                     >
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                                         </svg>
                                     </button>
                                 </div>
@@ -114,8 +114,8 @@ export default function ContactPage() {
                                             onClick={() => fnsetBookingMessage("")}
                                             className="absolute top-2 right-2 text-green-700 hover:text-green-900"
                                         >
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                                             </svg>
                                         </button>
                                     </div>
