5abfc94b4a3a7537ca7bf12e46fd8eedafed6113
Meharaj Thoufik <meharaj.thoufik@lmnas.com>
2025-04-21 16:13:29 +0530

    adding career

--- app/trending-now/page.tsx
+++ app/trending-now/page.tsx
@@ -3,7 +3,6 @@ import TrendingNowPage from "./trending-now";
 export default async function TrendingPage() {
   const ioTransformer: Itransformer<TtrendsPageSource, TtrendsPageTarget> = clTransformerFactory.createTransformer("Trends");
   const pageData: TtrendsPageTarget = await ioTransformer.execute({ locale: 'de-DE' });
-  console.log('Trends Page Data', pageData.trend.heroSection)
   return (
     <TrendingNowPage idTrend={pageData} />
   );
