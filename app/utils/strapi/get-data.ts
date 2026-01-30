e72a739338b50d6da8de21366a3b52e1388f152a
Krishna <krishnagovarthini-lmnas>
2026-01-30 14:30:34 +0530

    increased revalidation time

--- app/utils/strapi/get-data.ts
+++ app/utils/strapi/get-data.ts
@@ -36,7 +36,7 @@ export async function fnGetCacheData<DynamicSourceType, DynamicTargetType>(
       },
       [LCacheKey],
       {
-        revalidate: 10,
+        revalidate: 3600,
         tags: slug ? [LCacheKey, locale, slug] : [LCacheKey, locale],
       },
     )
