99f3742e509c69e382af859bac37a9d98a43aa6a
EMP24006 <ayesa.farsana@lmnas.com>
2026-07-06 15:49:27 +0530

    corrected the comment line

--- app/api/auth/google/route.ts
+++ app/api/auth/google/route.ts
@@ -1,4 +1,3 @@
-// Inside your Next.js API route file (e.g., /app/api/auth/google/route.ts)
 import { loginViaGoogle } from '@repo/ui/api/auth/google';
 
 export async function GET(request:Request) {
